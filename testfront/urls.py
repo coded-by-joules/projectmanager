a0e3fdcbac0810e5340319fcd1eacb3d474f039a
Jules Kelly Siarez <thephoenixtwister@gmail.com>
2023-10-09 22:12:50 +0800

    updated project list and created project view

--- testfront/urls.py
+++ testfront/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from . views import mainpage
+from . views import mainpage, projectview
 
 urlpatterns = [
-    path('', mainpage)
+    path('', mainpage, name="projectall"),
+    path('project/<project_ord>', projectview, name="project")
 ]
--- testfront/views.py
+++ testfront/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from api.models import Project
+from urllib.parse import unquote
 
 # Create your views here.
 def mainpage(request):
     projects = Project.objects.all()
-    return render(request, "testfront/index.html", {
-        "projects": projects
+    return render(request, "testfront/projectlist.html", {
+        "projects": projects,
+        "count": len(projects)
+    })
+
+def projectview(request, project_ord):
+    project = Project.objects.get(ord=project_ord)
+    return render(request, "testfront/project.html", {
+        "project": project
     })
