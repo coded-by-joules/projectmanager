a4d09b74c5d653a35d13d8c2566d1a80a7d3e954
Jules Kelly Siarez <juleskellysiarez@gmail.com>
2023-10-17 21:35:24 +0800

    added delete page and pagination

--- testfront/urls.py
+++ testfront/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from . views import mainpage, newproject, projectview, projectedit
+from . import views
 
 urlpatterns = [
-    path('', mainpage, name="projectall"),
-    path('project/new', newproject, name="newproject"),
-    path('project/<project_ord>', projectview, name="project"),
-    path('project/edit/<project_ord>', projectedit, name="projectedit")
+    path('', views.mainpage, name="projectall"),
+    path('project/new', views.newproject, name="newproject"),
+    path('project/<project_ord>', views.projectview, name="project"),
+    path('project/edit/<project_code>', views.projectedit, name="projectedit"),
+    path('project/delete/<project_code>', views.projectdel, name="projectdel")
 ]
--- testfront/views.py
+++ testfront/views.py
@@ -2,18 +2,68 @@ from django.shortcuts import render, redirect
 from api.models import Project
 from .forms import ProjectForm
 from django.contrib import messages
-
+from django.shortcuts import get_object_or_404
+import re
+from django.core.paginator import Paginator
 
 # Create your views here.
 def mainpage(request):
-    projects = Project.objects.all()
+    if "search" in request.GET:
+        searchTxt = str(request.GET['search']).strip()
+        if searchTxt.strip() == "":
+            projects_list = Project.objects.all()
+        else:
+            # check if search matches ord
+            try:
+                if re.search("^ORD-[0-9]{6}-[0-9A-Z]{4}$", searchTxt):
+                    projects_list = Project.objects.get(ord=searchTxt)
+                else:
+                    projects_list = Project.objects.filter(ord__icontains=searchTxt) | Project.objects.filter(project_name__icontains=searchTxt)
+            except Exception:
+                projects_list = [] # return empty if not found
+    else:
+        projects_list = Project.objects.all()
+
+    project_pages = Paginator(projects_list, 15)
+    page_num = 1
+    if "page" in request.GET:
+        if str(request.GET['page']).isdigit():
+            page_num = int(request.GET['page'])
+            if page_num > project_pages.num_pages:
+                page_num =  project_pages.num_pages
+    
+    min_page = page_num - 2
+    if min_page <= 1:
+        min_page = 1
+    max_page = min_page + 4
+    if max_page >= project_pages.num_pages:
+        max_page = project_pages.num_pages
+
+
+    project_page = project_pages.page(page_num)
+    
+    prev_page = 1
+    if project_page.has_previous():
+        prev_page = page_num - 1
+    next_page = project_pages.num_pages
+    if project_page.has_next():
+        next_page = page_num + 1
+        
     return render(request, "testfront/projectlist.html", {
-        "projects": projects,
-        "count": len(projects)
+        "projects": project_page.object_list,
+        "pages": range(min_page, max_page+1),
+        "last": project_pages.num_pages,
+        "current": project_page.number,
+        "showPrev": project_page.has_previous(),
+        "showNext": project_page.has_next(),
+        "prev_page": prev_page,
+        "next_page": next_page
     })
+    
+    
 
 def projectview(request, project_ord):
-    project = Project.objects.get(ord=project_ord)
+    project = get_object_or_404(Project, ord=project_ord)
     return render(request, "testfront/project.html", {
         "project": project
     })
@@ -38,8 +88,8 @@ def newproject(request):
             "formText": "Add"
            })
 
-def projectedit(request, project_ord):    
-    editproj = Project.objects.get(ord=project_ord)
+def projectedit(request, project_code):    
+    editproj =get_object_or_404(Project, code=project_code)
     if request.method == "GET":
         form = ProjectForm(instance = editproj, method="edit")
         return render(request, "testfront/formproject.html", {
@@ -58,3 +108,13 @@ def projectedit(request, project_ord):
             "form": form,
             "formText": "Edit"
            })
+
+def projectdel(request, project_code):    
+    project = get_object_or_404(Project, code=project_code)
+    if (request.method == "GET"):
+        return render(request, "testfront/formdelete.html", {
+            "project": project
+        })
+    else:
+        project.delete()
+        return redirect("projectall")
